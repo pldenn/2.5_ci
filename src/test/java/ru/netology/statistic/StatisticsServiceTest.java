54c1533e36efcd8a9636ae4529342182aca5d4d4
Denis Timofeev <y719xy@gmail.com>
2020-05-04 16:53:27 +0300

    Update test findMax. Jacoco check = success

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -7,14 +7,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class StatisticsServiceTest {
 
   @Test
-  void findMax() {
+  void findMax1() {
     StatisticsService service = new StatisticsService();
 
-    long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+    long[] incomesInBillions = {1, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
     long expected = 12;
 
     long actual = service.findMax(incomesInBillions);
 
     assertEquals(expected, actual);
   }
+
 }
